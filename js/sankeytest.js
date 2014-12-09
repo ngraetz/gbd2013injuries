65c6b64ba539f3ddb30c3c0461361d618a13052a
ngraetz <ngraetz@uw.edu>
2014-12-09 10:18:37 -0800

    update

--- js/sankeytest.js
+++ js/sankeytest.js
@@ -64,7 +64,7 @@ var nodeTextFunc = function(node) {
 };
 
 draw = function(url) {
-	d3.json(url, function(rdp) {	
+	$.getJSON(url, function(rdp) {	
 	
 		var nodeMap = {};
 		rdp.nodes.forEach(function(x) { nodeMap[x.name] = x; });
